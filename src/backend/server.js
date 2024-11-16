45a576e419d074d4965233b3af62c5ebe3696768
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-11-16 23:25:53 +0100

    add server

---
+++ src/backend/server.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('chat_message', (data) => {
+    console.log('Message received:', data);
+    io.emit('chat_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
